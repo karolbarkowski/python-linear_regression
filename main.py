9f6f444a800f5352dcc5e7f1f4e7bb486b7ab383
karol.barkowski <karol.barkowski@gmail.com>
2025-11-29 23:55:29 +0100

    added template method to call both methods

---
+++ main.py
@@ -0,0 +1,19 @@
+from enum import Enum
+from models.data_params import DataGenerationParams
+from regression_steps import perform_regression
+
+class Mode(Enum):
+    SKLEARN = "sklearn"
+    MANUAL = "manual"
+
+process_config = DataGenerationParams(
+    n_samples=100,
+    slope=2,
+    intercept=1,
+    noise_std=1,
+    random_seed=42,
+    training_fraction=0.8
+)
+
+perform_regression(process_config, mode=Mode.MANUAL.value)
+perform_regression(process_config, mode=Mode.SKLEARN.value)
---
+++ regression_steps.py
@@ -0,0 +1,38 @@
+from models import DataGenerationParams
+from steps import generate_linear_data, split_data, train_manual, train_sklearn, evaluate_model, visualize
+
+
+def perform_regression(process_config: DataGenerationParams, mode: str = "manual"):
+    # GENERATING SAMPLE DATA
+    X, Y = generate_linear_data(process_config)
+
+    print(f"Generated {len(X)} data points for equation: y = {process_config.slope}x + {process_config.intercept}")
+    print()
+
+    # SPLITTING DATA
+    X_train, y_train, X_test, y_test = split_data(X, Y, process_config.training_fraction)
+
+    print(f"Training set size: {len(X_train)} samples")
+    print(f"Testing set size: {len(X_test)} samples")
+    print()
+
+    # TRAINING THE MODEL
+    match mode:
+        case "manual":
+            model, learned_slope, learned_intercept = train_manual(X_train, y_train, n_iterations=1000, learning_rate=0.01)
+        case "sklearn":
+            model, learned_slope, learned_intercept = train_sklearn(X_train, y_train)
+        case _:
+            raise ValueError(f"Unknown mode: {mode}")
+
+    print(f"Learned equation: y = {learned_slope:.2f}x + {learned_intercept:.2f}")
+    print()
+
+    # PREDICTIONS
+    y_test_pred = model.predict(X_test)
+
+    # MODEL EVALUATION
+    test_mse, test_r2 = evaluate_model(y_test, y_test_pred)
+
+    # VISUALIZATIONS
+    visualize(X, Y, X_train, y_train, X_test, y_test, y_test_pred, learned_slope, learned_intercept, process_config)
